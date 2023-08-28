ae075f1c981d0613d11adc2c22d9d7f0c47a7385
yukogan <yuriknor@gmail.com>
2023-08-28 15:26:31 +0200

    Generated

---
+++ src/main/java/org/stht/hastemod/HasteMod.java
@@ -0,0 +1,13 @@
+package org.stht.hastemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HasteMod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/stht/hastemod/client/HasteModClient.java
@@ -0,0 +1,13 @@
+package org.stht.hastemod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HasteModClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
