aa4859e699cda4e87076e78ca21a910a9229b6c7
yukogan <yuriknor@gmail.com>
2023-08-28 22:28:31 +0200

    Generated

---
+++ src/main/java/org/stht/hastemod/client/BlockBreaker.java
@@ -0,0 +1,65 @@
+package org.stht.hastemod.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+import java.util.Objects;
+
+public class BlockBreaker {
+    private final Direction direction = Direction.UP;
+    private Block lastMinedBlock = null;
+    private boolean enabled = false;
+    private final int range = 4;
+
+    @Environment(EnvType.CLIENT)
+    public void tryBreak(BlockPos blockPos, MinecraftClient client) {
+        if (!client.world.getBlockState(blockPos).isAir() && Objects.equals(client.world.getBlockState(blockPos).getBlock(), lastMinedBlock)) {
+            client.interactionManager.attackBlock(blockPos, direction);
+            client.interactionManager.updateBlockBreakingProgress(blockPos, this.direction);
+        }
+    }
+
+    public void onTick(MinecraftClient client) {
+        if (client.player == null) return;
+        if (HasteModClient.getToggleKey().wasPressed() && !client.isPaused()) {
+            enabled = !enabled;
+            if (enabled) {
+                client.player.sendMessage(Text.of("§eToggled HasteMod to §aon"));
+            } else {
+                client.player.sendMessage(Text.of("§eToggled HasteMod to §coff"));
+                return;
+            }
+        }
+        if (lastMinedBlock == null || !enabled) return;
+        if (HasteModClient.getActivateKey().isPressed()) {
+            for (int x = client.player.getBlockPos().getX() - range; x < client.player.getBlockPos().getX() + range; x++) {
+                for (int y = client.player.getBlockPos().getY(); y < client.player.getBlockPos().getY() + range; y++) {
+                    for (int z = client.player.getBlockPos().getZ() - range; z < client.player.getBlockPos().getZ() + range; z++) {
+                        BlockPos nbp = new BlockPos(x, y, z);
+                        tryBreak(nbp, client);
+                    }
+                }
+            }
+        }
+    }
+
+    public void onBlockBreak(BlockState state, MinecraftClient client) {
+        if (client.player == null) return;
+        if (client.player.getUuid() != MinecraftClient.getInstance().player.getUuid()) return;
+        if (updateBlock(state.getBlock())) {
+            client.player.sendMessage(Text.of("§eSelected block: §3" + state.getBlock().getName().getString()));
+        }
+    }
+
+    public boolean updateBlock(Block block) {
+        if (block == this.lastMinedBlock || !enabled) return false;
+        this.lastMinedBlock = block;
+        return true;
+    }
+}
--- src/main/java/org/stht/hastemod/client/HasteModClient.java
+++ src/main/java/org/stht/hastemod/client/HasteModClient.java
@@ -1,13 +1,49 @@
 package org.stht.hastemod.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class HasteModClient implements ClientModInitializer {
-    /**
-     * Runs the mod initializer on the client environment.
-     */
+    private static KeyBinding useKey;
+    private static KeyBinding toggleKey;
+
     @Override
     public void onInitializeClient() {
+        useKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Use",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_X,
+                "HasteMod"
+        ));
 
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Toggle on/off",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_U,
+                "HasteMod"
+        ));
+
+
+        BlockBreaker bbreaker = new BlockBreaker();
+        ClientTickEvents.END_CLIENT_TICK.register((tick) -> bbreaker.onTick(MinecraftClient.getInstance()));
+        PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, entity) -> bbreaker.onBlockBreak(state, MinecraftClient.getInstance()));
+
+        System.out.println("Initialized HasteMod");
     }
+
+    public static KeyBinding getActivateKey() {
+        return useKey;
+    }
+
+    public static KeyBinding getToggleKey() {
+        return toggleKey;
+    }
+
 }
