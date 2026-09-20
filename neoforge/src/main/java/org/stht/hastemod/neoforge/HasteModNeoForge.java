b307c5990b39d12df4ebf2f643902b423ccfa12f
Yuri Koganovitsch <yuri@stht.org>
2026-09-21 00:46:05 +0200

    NeoForge support

--- neoforge/src/main/java/org/stht/hastemod/neoforge/HasteModNeoForge.java
+++ neoforge/src/main/java/org/stht/hastemod/neoforge/HasteModNeoForge.java
@@ -3,33 +3,39 @@ package org.stht.hastemod.neoforge;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
-import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.stht.hastemod.HasteMod;
 import org.stht.hastemod.client.HasteModClient;
+import org.stht.hastemod.client.config.HasteConfigScreen;
 
 @Mod(HasteMod.MOD_ID)
 public class HasteModNeoForge {
-    public HasteModNeoForge(IEventBus modEventBus) {
+    public HasteModNeoForge(IEventBus modEventBus, ModContainer modContainer) {
+        if (FMLEnvironment.getDist().isClient()) {
+            modContainer.registerExtensionPoint(
+                    IConfigScreenFactory.class,
+                    (container, parentScreen) -> HasteConfigScreen.build(parentScreen)
+            );
+        }
     }
 
-    @EventBusSubscriber(modid = HasteMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
+    @EventBusSubscriber(modid = HasteMod.MOD_ID, value = Dist.CLIENT)
+    public static class ClientEvents {
         @SubscribeEvent
         public static void onKeyRegister(RegisterKeyMappingsEvent event) {
             new HasteModClient().onInitializeClient();
-            
+
             for (var key : NeoForgePlatformHelper.KEY_MAPPINGS) {
                 event.register(key);
             }
         }
-    }
 
-    @EventBusSubscriber(modid = HasteMod.MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
-    public static class ClientGameEvents {
         @SubscribeEvent
         public static void onClientTick(ClientTickEvent.Post event) {
             for (var tickEvent : NeoForgePlatformHelper.TICK_EVENTS) {
